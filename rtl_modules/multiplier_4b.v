9be0e199481f22f8b9787392eef42b898822cc42
MarziehLenjani <ml2au@virginia.edu>
2021-11-03 22:07:29 -0500

    Add files via upload

---
+++ rtl_modules/multiplier_4b.v
@@ -0,0 +1,20 @@
+module multiplier #(
+   parameter BIT_SZ = 4
+)(
+   input  logic              rst_l,
+   input  logic              clk,
+   input  logic [BIT_SZ-1:0] a,
+   input  logic [BIT_SZ-1:0] b,
+   output logic [BIT_SZ-1:0] y
+);
+
+
+logic [(2*BIT_SZ)-1:0] y_temp;
+
+assign y_temp=a*b;
+
+always @(posedge clk or negedge rst_l)
+  if (~rst_l) y <= 'h0;
+  else        y <= y_temp[BIT_SZ-1:0]; //FIXME: verify this 
+
+endmodule
